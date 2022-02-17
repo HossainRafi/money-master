16f0ad4db7546f728acef853e59f830d460902b7
HossainRafi <hossainrafi71@gmail.com>
2022-02-17 16:31:14 +0600

    dom

--- script/script.js
+++ script/script.js
@@ -2,20 +2,27 @@ document.getElementById("calculate").addEventListener("click", function () {
   //Get income balance
   const myIncomeInput = document.getElementById("income");
   const myIncome = myIncomeInput.value;
-  console.log(myIncome);
 
   // Get food cost
   const myFoodCostInput = document.getElementById("food-cost");
   const myFoodCost = myFoodCostInput.value;
-  console.log(myFoodCost);
 
   // Get rent cost
   const myRentCostInput = document.getElementById("rent-cost");
   const myRentCost = myRentCostInput.value;
-  console.log(myRentCost);
 
   // Get cloth cost
   const myClothCostInput = document.getElementById("cloth-cost");
   const myClothCost = myClothCostInput.value;
-  console.log(myClothCost);
+
+  // ============================
+  const myTotalCost =
+    Number(myFoodCost) + Number(myRentCost) + Number(myClothCost);
+
+  // ====================
+  const myExpense = document.getElementById("cost");
+  myExpense.innerText = myTotalCost;
+
+  const Balance = document.getElementById("remain-balance");
+  Balance.innerText = Number(myIncome) - myTotalCost;
 });
